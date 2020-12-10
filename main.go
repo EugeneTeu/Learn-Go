7ba4257ab5242b2f84e0f6382ef8d7c00c749dd6
eugene teu <eugeneteu@gmail.com>
2020-12-10 23:13:36 +0800

    fix formating issue

--- main.go
+++ main.go
@@ -12,29 +12,30 @@ import (
 
 // Row struct
 type Row struct {
-	ID	 string `json:"Id"`;
-	Name string `json:"Name"`;
+	ID   string `json:"Id"`
+	Name string `json:"Name"`
 }
-var arr []Row;
+
+var arr []Row
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the home page");
-	fmt.Printf("endpoint: home page");
+	fmt.Fprintf(w, "Welcome to the home page")
+	fmt.Printf("endpoint: home page")
 
 }
 
 func retriveRows(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllRows")
-    json.NewEncoder(w).Encode(arr);
+	json.NewEncoder(w).Encode(arr)
 }
 
 func retriveSingleRow(w http.ResponseWriter, r *http.Request) {
-	variables := mux.Vars(r);
-	key := variables["id"];
+	variables := mux.Vars(r)
+	key := variables["id"]
 	//fmt.Fprintf(w, "key: " + key);
 	for _, row := range arr {
 		if row.ID == key {
-			json.NewEncoder(w).Encode(row);
+			json.NewEncoder(w).Encode(row)
 		}
 	}
 }
@@ -43,59 +44,59 @@ func createSingleRow(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var row Row
 	//fmt.Fprintf(w, "%+v", string(reqBody))
-	json.Unmarshal(reqBody, &row);
+	json.Unmarshal(reqBody, &row)
 
-	arr = append(arr, row);
-	json.NewEncoder(w).Encode(arr);
+	arr = append(arr, row)
+	json.NewEncoder(w).Encode(arr)
 	//fmt.Fprintf(w, "%+v", string(reqBody))
 }
 
 func updateSingleRow(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: update single Row")
-	id := mux.Vars(r)["id"];
+	id := mux.Vars(r)["id"]
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var newRow Row
-	json.Unmarshal(reqBody, &newRow);
+	json.Unmarshal(reqBody, &newRow)
 
 	for index, row := range arr {
 		if row.ID == id {
-			row.Name = newRow.Name;
-			arr[index] = row;
+			row.Name = newRow.Name
+			arr[index] = row
 		}
 	}
 
 }
 
 func deleteSingleRow(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"];
+	id := mux.Vars(r)["id"]
 	for index, row := range arr {
 		if row.ID == id {
-			arr = append(arr[:index],arr[index+1:]... )
+			arr = append(arr[:index], arr[index+1:]...)
 		}
 	}
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage);
-	http.HandleFunc("/row", retriveRows);
-	log.Fatal(http.ListenAndServe(":10000", nil));
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/row", retriveRows)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func handleRequestsWithRouter() {
-	myRouter := mux.NewRouter().StrictSlash(true);
-	myRouter.HandleFunc("/", homePage);
-	myRouter.HandleFunc("/all", retriveRows);
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", retriveRows)
 	myRouter.HandleFunc("/row", createSingleRow).Methods("POST")
 	myRouter.HandleFunc("/row/{id}", deleteSingleRow).Methods("DELETE")
 	myRouter.HandleFunc("/row/{id}", updateSingleRow).Methods("PUT")
-	myRouter.HandleFunc("/row/{id}", retriveSingleRow);
-	log.Fatal((http.ListenAndServe(":10000", myRouter)));
+	myRouter.HandleFunc("/row/{id}", retriveSingleRow)
+	log.Fatal((http.ListenAndServe(":10000", myRouter)))
 }
 
 func main() {
 	arr = []Row{
-		{ID: "1" , Name: "alpha"},
-		{ID: "2" , Name: "beta"},
+		{ID: "1", Name: "alpha"},
+		{ID: "2", Name: "beta"},
 	}
-	handleRequestsWithRouter();
+	handleRequestsWithRouter()
 }
