8ab8a167b3a7a7f6d23b6c3e095925d424c00e20
eugene teu <eugeneteu@gmail.com>
2020-12-13 17:27:05 +0800

    added db link

--- goMySql/main.go
+++ goMySql/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
 	fmt.Println("Go and MySql tutorial")
+
+	db, err := sql.Open("mysql", "eugeneteu:Acjc2014@tcp(127.0.0.1:3306)/G0_SQL")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 }
