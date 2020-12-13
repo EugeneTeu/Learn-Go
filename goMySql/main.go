168dc3bb5fb2718a45f652b2c8a3355a262dc005
eugene teu <eugeneteu@gmail.com>
2020-12-13 22:22:43 +0800

    simple insert statement

--- goMySql/main.go
+++ goMySql/main.go
@@ -9,15 +9,28 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Database name = GO_SQL
+// Table: test
+// CREATE TABLE test ( Num int, Name varchar(255));
+
 func main() {
 	err := godotenv.Load()
 	fmt.Println("Go and MySql tutorial")
-
+	//fmt.Println(os.Getenv("dev"))
 	db, err := sql.Open("mysql", os.Getenv("dev"))
 	if err != nil {
 		panic(err.Error())
 	}
 
 	//fmt.Println(os.Environ())
+
 	defer db.Close()
+	insert, err := db.Query("INSERT INTO test VALUES (2,'TEST') ")
+	if err != nil {
+		//fmt.Println("inserrt error")
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+
 }
