8d26abd662c72060b0303807fa8ef745e2a161ab
eugene teu <eugeneteu@gmail.com>
2020-12-26 16:46:44 +0800

    add sql query execution on server startup

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ package main
 //TODO: Convert to mysql
 //TODO: Add function signatures
 import (
+	"database/sql"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
@@ -25,7 +28,7 @@ var rockets []Rocket
 func errorHandler(err error, message string) {
 	if err != nil {
 		log.Println(message)
-		panic(err.Error)
+		panic(err.Error())
 	}
 }
 
@@ -37,6 +40,24 @@ func main() {
 	log.Println("Starting Server")
 	err := godotenv.Load()
 	errorHandler(err, "error with loading env variables")
+	db, err := sql.Open("mysql", os.Getenv("dev"))
+	errorHandler(err, "error with db connection")
+
+	// run sql create table
+	dropTable, err := ioutil.ReadFile("./sql/drop-table.sql")
+	errorHandler(err, "error reading sql file")
+	//log.Printf(string(query))
+	db.Query(string(dropTable))
+	createTable, err := ioutil.ReadFile("./sql/create-table.sql")
+	//log.Printf(string(createTable))
+	_, err = db.Query(string(createTable))
+	errorHandler(err, "create table failed")
+	/*
+		_, err = db.Exec(string(query))
+		errorHandler(err, "error executing sql file")
+		//fmt.Println(os.Environ())
+	*/
+	defer db.Close()
 	// init router singleton
 	myRouter := Router()
 	log.Printf("Running on port %v\n", os.Getenv("PORT"))
---
+++ sql/create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Rocket` (
+    `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+    `rocket_name` varchar(30) NOT NULL,
+    `payload_weight` varchar(30) NOT NULL,
+    `rocket_type` varchar(30) NOT NULL,
+    PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=1;
